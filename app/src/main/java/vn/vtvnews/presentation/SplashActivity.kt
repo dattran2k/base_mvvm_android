4259214e6dae94b8b310c5df964c66619a5a6989
dattran <dattran@vccorp.vn>
2023-02-03 15:30:04 +0700

    [ UPDATE ] fix plash

--- app/src/main/java/vn/vtvnews/presentation/SplashActivity.kt
+++ app/src/main/java/vn/vtvnews/presentation/SplashActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import vn.vtvnews.databinding.ActivitySplashBinding
+import vn.vtvnews.databinding.M00ActivityMainBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(ActivitySplashBinding.inflate(layoutInflater).root)
         Handler(Looper.getMainLooper()).postDelayed({
             goToMainActivity()
         }, 1000)
