6c71e788696c87078b895a73895e320e3bd7f5fe
dattran <dattran@vccorp.vn>
2023-05-29 15:40:00 +0700

    update base

--- app/src/main/java/com/base/data/model/HomeTabModel.kt
+++ app/src/main/java/com/base/data/model/HomeTabModel.kt
@@ -2,9 +2,10 @@ package com.base.data.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
 import kotlin.reflect.KClass
 
-data class HomeTabModel<T : Any>(
+data class HomeTabModel<T : Fragment>(
     val fragment: KClass<T>,
     @DrawableRes val icon: Int,
     @StringRes val title: Int
--- app/src/main/java/com/base/presentation/view/main/m01_home/M01HomeViewModel.kt
+++ app/src/main/java/com/base/presentation/view/main/m01_home/M01HomeViewModel.kt
@@ -6,26 +6,33 @@ import com.base.data.respository.DemoRepository
 import com.base.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
-import kotlin.concurrent.thread
 
+/** Why ViewModel state  difference than this project from Google : https://github.com/dattran2k/nowinandroid/tree/main/feature
+ * =======================================================================================
+ * Data  of "now in android" project is listen from local (Room, DataStore) using Flow
+ * check https://developer.android.com/topic/libraries/architecture/datastore?hl=vi
+ * and https://developer.android.com/codelabs/basic-android-kotlin-training-intro-room-flow?hl=vi#0
+ * that's mean if database data changed, flow will emit new data, and ViewModel state ( homeUiState ) will able to collect that
+ * and if you want to refresh new data ?
+ * 1.UI action ->
+ * 2.Request API for new data ->
+ * 3.New data will be save in Room or DataStore ->
+ * 4.Flow Room or DataStore will emit new value ->
+ * 5.Flow in ViewModel that collect from that flow will map and produce new UI state .
+ * Check out : https://github.com/dattran2k/nowinandroid/blob/main/feature/foryou/src/main/java/com/google/samples/apps/nowinandroid/feature/foryou/ForYouViewModel.kt
+ * That's why it difference. One day, maybe I will create that data flow
+ * =========================================================================================
+ */
 @HiltViewModel
 class M01HomeViewModel @Inject constructor(val demoRepository: DemoRepository) : BaseViewModel() {
 
     private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
     val homeUiState: StateFlow<HomeUiState> = _homeUiState
+
     init {
         getData()
     }
