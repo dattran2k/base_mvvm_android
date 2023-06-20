9f7b6e073f98cb1c1529db1e176b5b0d3b37caac
dattran <dattran@vccorp.vn>
2023-06-20 12:11:28 +0700

    optimize

--- app/src/main/java/com/base/core/common/Constants.kt
+++ app/src/main/java/com/base/core/common/Constants.kt
@@ -1,10 +1,7 @@
 package com.base.core.common
 
-import androidx.datastore.preferences.core.intPreferencesKey
-
 object Constants {
     const val FLAVOR_DEVELOP = "development"
     const val FLAVOR_PRODUCT = "production"
-    const val PREFERENCES_STORE_NAME = "DemoPref"
 
 }
